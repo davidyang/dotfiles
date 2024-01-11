68e02a065eec4caa61b2981094648936b5567ccf
david yang <david.g.yang@gmail.com>
2024-01-11 13:29:38 -0500

    updates

---
+++ bin/backup-dropbox-to-restic.sh
@@ -0,0 +1,4 @@
+export AWS_ACCESS_KEY_ID=000dd6fa4574a4a0000000002 
+export AWS_SECRET_ACCESS_KEY=K0005VGwq1PEx50JZgME+zJqLxNVLxI 
+
+restic -r s3:s3.us-west-000.backblazeb2.com/dropbox-sync-restic backup /Users/dyang/Library/CloudStorage/Dropbox
